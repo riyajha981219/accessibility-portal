1c17cdb170a64139185ddfb67e9e456e8f3f817e
Riya Jha <jhariya.1912@gmail.com>
2025-05-27 20:24:34 +0530

    - summarization of text files added and clean response structured

--- backend/routes/process.py
+++ backend/routes/process.py
@@ -1,14 +1,32 @@
 from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse
 from services.file_parser import parse_file
 from services.summarizer import get_summary_from_gemini
 
 router = APIRouter()
 
-@router.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    content = await parse_file(file)
-    summary = get_summary_from_gemini(content)
-    return {
-        "filename": file.filename,
-        "summary": summary
-    }
+@router.post("/summarize-file")
+async def summarize_file(file: UploadFile = File(...)):
+    try:
+        text = await parse_file(file)
+
+        if text.strip().lower() == "unsupported file type":
+            return {"error": "Unsupported file type"}
+
+        summary_result = get_summary_from_gemini(text)
+        return JSONResponse(
+                status_code=200,
+                content={
+                    "status": "success",
+                    "filename": file.filename,
+                    "summary": summary_result
+                }
+            )
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status": "error",
+                "message": str(e)
+            }
+        )
